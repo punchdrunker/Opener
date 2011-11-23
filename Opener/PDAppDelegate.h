32c5102457b91f9a468782acb83d379a5fa68404
nanao <nanao@pd-book.local>
2011-11-23 19:08:39 +0900

    とりあえずファイルをFinderで開けるようにした

--- Opener/PDAppDelegate.h
+++ Opener/PDAppDelegate.h
@@ -3,13 +3,21 @@
 //  Opener
 //
 //  Created by nanao on 11/11/23.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 punchdrunker.org. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 
-@interface PDAppDelegate : NSObject <NSApplicationDelegate>
+@interface PDAppDelegate : NSObject <NSApplicationDelegate>{
+    NSTextField *urlField;
+    NSTextField *errorField;
+    NSButton *openButoon;
+}
 
 @property (assign) IBOutlet NSWindow *window;
+@property (nonatomic, retain) IBOutlet NSTextField *urlField;
+@property (nonatomic, retain) IBOutlet NSTextField *errorField;
+@property (nonatomic, retain) IBOutlet NSButton *openButoon;
 
+-(IBAction)openButtonPushed:(id)sender;
 @end
--- Opener/PDAppDelegate.m
+++ Opener/PDAppDelegate.m
@@ -3,7 +3,7 @@
 //  Opener
 //
 //  Created by nanao on 11/11/23.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 punchdrunker.org. All rights reserved.
 //
 
 #import "PDAppDelegate.h"
@@ -11,15 +11,32 @@
 @implementation PDAppDelegate
 
 @synthesize window = _window;
+@synthesize urlField, openButoon, errorField;
 
-- (void)dealloc
-{
+- (void)dealloc {
+    urlField = nil;
+    openButoon = nil;
+    errorField = nil;
+    
     [super dealloc];
 }
 	
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
 }
 
+-(IBAction)openButtonPushed:(id)sender {
+    NSLog(@"%s", __func__);
+    
+    if ([[NSFileManager defaultManager] fileExistsAtPath:urlField.stringValue]) {
+        //errorField.stringValue = urlField.stringValue;
+        NSString *filePath = [urlField.stringValue stringByExpandingTildeInPath];
+        NSString *windowPath = [@"~/Library" stringByExpandingTildeInPath];
+        [[NSWorkspace sharedWorkspace] selectFile:urlField.stringValue 
+                         inFileViewerRootedAtPath:windowPath ];
+    }
+    else {
+        [errorField setStringValue:@"無効なファイルパスです"];
+    }
+}
 @end
